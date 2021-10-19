145ddbb53a47f7c84dc3360c3d87dee52cb20b1f
aneesh <aneesh.v.edara@gmail.com>
2021-10-19 13:53:18 -0400

    ComputerPlayer takeTurn 2/3 complete

--- Player.py
+++ Player.py
@@ -17,11 +17,11 @@ class Player:
     # This is a useful method to determine if the space is "~" or something else
     # Send it the grid you want to check, so ship or shot
     def createShipGrid(self):
-        placeShip("A", 5 )
-        placeShip( "B", 4 )
-        placeShip( "C", 3 )
-        placeShip( "S", 3 )
-        placeShip( "D", 2 )
+        self.placeShip("A", 5 )
+        self.placeShip( "B", 4 )
+        self.placeShip( "C", 3 )
+        self.placeShip( "S", 3 )
+        self.placeShip( "D", 2 )
 
     def printGrids(self):
         print("Ship Grid")
--- computerplayertest.py
+++ computerplayertest.py
@@ -1,5 +1,6 @@
 from ComputerPlayer import ComputerPlayer
 
 comp_player = ComputerPlayer()
+#comp_player.placeShip("A", 5)
 comp_player.createShipGrid()
 comp_player.printGrids()
