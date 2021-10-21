ae3ea56d9426f404aa7b6e4f5029053ccf6dc5a2
aneesh <aneesh.v.edara@gmail.com>
2021-10-21 09:40:41 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- Game.py
+++ Game.py
@@ -1,1 +1,17 @@
-from Computer
+from ComputerPlayer import ComputerPlayer
+from humanPlayer import humanPlayer
+class Game:
+    def __init__(self, cp, hp):
+        pass
+
+    def Play(self, cp, hp):
+        while cp.stillHasShips() and hp.stillHasShips():
+            cp.createShipGrid()
+            hp.createShipGrid()
+
+
+
+
+
+
+
